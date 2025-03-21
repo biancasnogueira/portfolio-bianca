f39b18c8dce841e18b1eaf3d5d1c4324e3b174c8
Bianca Nogueira <nogbianca@outlook.com>
2025-03-21 10:44:52 -0300

    feat(frontend): versão inicial do projeto

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Portfólio de desenvolvimento web",
+};
+
+const fonte = Montserrat({
+  subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${fonte.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
